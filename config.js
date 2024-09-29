f715fb516a442b862aa2018bf4a3be4c69074fd0
Ben-kerm <hanos7275@gmail.com>
2024-09-29 12:36:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ module.exports = {
    AUTO_REACT: toBool(process.env.AUTO_REACT) || false,
 
    // String Values
-   SESSION_ID: process.env.SESSION_ID || "Session~vEixED6Y", // Put your session
+   SESSION_ID: process.env.SESSION_ID || "Session~fhqViND8", // Put your session
    HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "[.]",
    BOT_INFO: process.env.BOT_INFO || "Kɢᴛᴇᴄʜ;KᴇʀᴍLɪᴛᴇ⚡️", // YourName;BotName;Image/VideoLink
-   SUDO: process.env.SUDO || "14103173641",
+   SUDO: process.env.SUDO || "50947567596",
    PRESENCE: process.env.PRESENCE || "available", // "unavailable", "available", "composing", "recording", "paused"
    CAPTION: process.env.CAPTION || "© Kᴇʀᴍ ᴍᴅ",
    TIME_ZONE: process.env.TIME_ZONE || "Africa/Lagos",
